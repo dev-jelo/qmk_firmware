68d715033f9c75293e7d9edb3722b6242a2a5747
Jelo <devjelo@gmail.com>
2025-02-12 22:33:15 +1100

    Add home row mods

--- keyboards/dactyl_tightyl/keymaps/default/keymap.c
+++ keyboards/dactyl_tightyl/keymaps/default/keymap.c
@@ -8,10 +8,12 @@
 #define NAV 2
 #define NUM 3
 
+// Tri layer
 #define TRI_LAYER_LOWER_LAYER 1 
 #define TRI_LAYER_UPPER_LAYER 2
 #define TRI_LAYER_ADJUST_LAYER 3 
 
+// One shot mods
 #define MRGUI OSM(MOD_RGUI)
 #define MRALT OSM(MOD_RALT)
 #define MRCTL OSM(MOD_RCTL)
@@ -22,12 +24,26 @@
 #define MLCTL OSM(MOD_LCTL)
 #define MLSFT OSM(MOD_LSFT)
 
+
+// Home row mods
+#define HRM_A LGUI_T(KC_A)
+#define HRM_S LALT_T(KC_S)
+#define HRM_D LCTL_T(KC_D)
+#define HRM_F LSFT_T(KC_F)
+
+#define HRM_J RSFT_T(KC_J)
+#define HRM_K RCTL_T(KC_K)
+#define HRM_L LALT_T(KC_L)
+#define HRM_SCN RGUI_T(KC_SCLN)
+
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
      * | TAB  |   q  |   w  |   e  |   r  |   t  |                          |   y  |   u  |   i  |   o  |   p  | BSPC |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
-     * | CAPS |   a  |   s  |   d  |   f  |   g  |                          |   h  |   j  |   k  |   l  |   ;  |   '  |
+     * | CAPS | a(G) | s(A) | d(C) | f(S) |   g  |                          |   h  | j(S) | k(C) | l(A) | ;(G) |   '  |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
      * | SHIFT|   z  |   x  |   c  |   v  |   b  |                          |   n  |   m  |   ,  |   .  |   /  | SHIFT|
      * '------'------+------+------+------+------'                          '------'------+------+------+------'------'
@@ -38,18 +54,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
 
     [BASE] = LAYOUT(
-         KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
-        KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                        KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
-        KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
-                          KC_LCTL, KC_LALT,                                                         KC_RCTL, KC_RALT,
-                                            TL_UPPR,  KC_SPC, KC_LGUI,    KC_ENT,  KC_SPC, TL_LOWR
+         KC_TAB,   KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+        KC_CAPS,  HRM_A,  HRM_S,   HRM_D,  HRM_F,   KC_G,                        KC_H,   HRM_J,   HRM_K,   HRM_L, HRM_SCN, KC_QUOT,
+        KC_LSFT,   KC_Z,   KC_X,    KC_C,   KC_V,   KC_B,                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+                        KC_LCTL, KC_LALT,                                                       KC_RALT, KC_RCTL,
+                                         TL_UPPR, KC_SPC, KC_LGUI,     KC_ENT, KC_SPC, TL_LOWR
     ),
 
     /* Symbol Layer
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
      * |   ~  |   +  |   &  |   *  |   (  |   {  |                          |   }  |   )  |   '  |   :  |   ;  | ____ |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
-     * |   `  |   =  |   $  |   %  |   ^  |   [  |                          |   ]  | META | ALT  | CTRL | SHIFT|   |  |
+     * |   `  |   =  |   $  |   %  |   ^  |   [  |                          |   ]  | SHIFT| CTRL |  ALT | META |   |  |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
      * |   _  |   -  |   !  |   @  |   #  |   <  |                          |   >  |   "  | ____ | ____ | ____ |   \  |
      * '------'------+------+------+------'------'                          '------'------+------+------+------'------'
@@ -61,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [SYM] = LAYOUT(
         KC_TILD, KC_PPLS, KC_AMPR, KC_ASTR, KC_LPRN, KC_LCBR,                     KC_RCBR, KC_RPRN, KC_QUOT, KC_COLN, KC_SCLN, _______,
-         KC_GRV, KC_PEQL,  KC_DLR, KC_PERC, KC_CIRC, KC_LBRC,                     KC_RBRC,   MRGUI,   MRALT,   MRCTL,   MRSFT, KC_PIPE,
+         KC_GRV, KC_PEQL,  KC_DLR, KC_PERC, KC_CIRC, KC_LBRC,                     KC_RBRC,   MRSFT,   MRCTL,   MRALT,   MRGUI, KC_PIPE,
         KC_UNDS, KC_MINS, KC_EXLM,   KC_AT, KC_HASH,   KC_LT,                       KC_GT,  KC_DQT, _______, _______, _______, KC_BSLS,
                           _______, _______,                                                         _______, _______,
                                             _______, _______, _______,   _______, _______, _______
@@ -71,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
      * |  ESC |      | PREV | PAUSE| NEXT |  VL+ |                          | PGUP | HOME |  UP  |  END |      | ____ |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
-     * | ALTAB| SHIFT| CTRL |  ALT | META |  VL- |                          | CLEFT| LEFT | DOWN | RIGHT| CRGHT|  DEL |
+     * | ALTAB|  GUI |  ALT | CTRL | SHIFT|  VL- |                          | CLEFT| LEFT | DOWN | RIGHT| CRGHT|  DEL |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
      * | PRTSC|      |      |      | STOP | MUTE |                          | PGDN | TABL |      |  TAB |      |  INS |
      * '------'------+------+------+------'------'                          '------'------+------+------+------'------'
@@ -83,7 +99,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NAV] = LAYOUT(
          KC_ESC,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                     KC_PGUP,   KC_HOME,   KC_UP,  KC_END,      KC_NO, _______,
-      A(KC_TAB),   MLSFT,   MLCTL,   MLALT,   MLGUI, KC_VOLD,                  C(KC_LEFT),   KC_LEFT, KC_DOWN, KC_RGHT, C(KC_RGHT),  KC_DEL,
+      A(KC_TAB),   MLGUI,   MLALT,   MLCTL,   MLSFT, KC_VOLD,                  C(KC_LEFT),   KC_LEFT, KC_DOWN, KC_RGHT, C(KC_RGHT),  KC_DEL,
         KC_PSCR,   KC_NO,   KC_NO,   KC_NO, KC_MSTP, KC_MUTE,                     KC_PGDN, S(KC_TAB),   KC_NO,  KC_TAB,      KC_NO,  KC_INS,
                           _______, _______,                                                         _______, _______,
                                             _______, _______, _______,   _______, _______, _______
@@ -93,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
      * |      |      |  F7  |  F8  |  F9  |  F10 |                          |   +  |   7  |   8  |   9  |   *  | ____ |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
-     * | NLCK | SHIFT|  F4  |  F5  |  F6  |  F11 |                          |   =  |   4  |   5  |   6  |   ,  |  DEL |
+     * | NLCK |      |  F4  |  F5  |  F6  |  F11 |                          |   =  |   4  |   5  |   6  |   ,  |  DEL |
      * |------+------+------+------+------+------|                          |------+------+------+------+------+------|
      * |      |      |  F1  |  F2  |  F3  |  F12 |                          |   -  |   1  |   2  |   3  |   /  |  ENT |
      * '------'------+------+------+------'------'                          '------'------+------+------+------'------'
@@ -105,7 +121,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [NUM] = LAYOUT(
           KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                     KC_PPLS,   KC_P7,   KC_P8,   KC_P9, KC_PAST, _______,
-         KC_NUM,   MLSFT,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                     KC_PEQL,   KC_P4,   KC_P5,   KC_P6, KC_COMM,  KC_DEL,
+         KC_NUM,   KC_NO,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                     KC_PEQL,   KC_P4,   KC_P5,   KC_P6, KC_COMM,  KC_DEL,
           KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                     KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PENT,
                           _______, _______,                                                           KC_P0, KC_PDOT,
                                             _______, _______, _______,   _______, _______, _______
