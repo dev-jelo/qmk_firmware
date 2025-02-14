edfa0faadf38f0c24fc72e64dad8cb316b89bf39
Jelo <devjelo@gmail.com>
2025-02-15 00:34:41 +1100

    Fix formatting

--- keyboards/dactyl_tightyl/keymaps/default/keymap.c
+++ keyboards/dactyl_tightyl/keymaps/default/keymap.c
@@ -52,7 +52,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             '------'------'------'            '------'------'------'
      */
 
-    [BASE] = LAYOUT(KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, KC_CAPS, HRM_A, HRM_S, HRM_D, HRM_F, KC_G, KC_H, HRM_J, HRM_K, HRM_L, HRM_SCN, KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LCTL, KC_LALT, KC_RALT, KC_RCTL, TL_UPPR, KC_SPC, KC_LGUI, KC_ENT, KC_SPC, TL_LOWR),
+    [BASE] = LAYOUT(
+         KC_TAB,   KC_Q,   KC_W,    KC_E,   KC_R,   KC_T,                        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,
+        KC_CAPS,  HRM_A,  HRM_S,   HRM_D,  HRM_F,   KC_G,                        KC_H,   HRM_J,   HRM_K,   HRM_L, HRM_SCN, KC_QUOT,
+        KC_LSFT,   KC_Z,   KC_X,    KC_C,   KC_V,   KC_B,                        KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
+                        KC_LCTL, KC_LALT,                                                       KC_RALT, KC_RCTL,
+                                         TL_UPPR, KC_SPC, KC_LGUI,     KC_ENT, KC_SPC, TL_LOWR
+    ),
 
     /* Symbol Layer
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
@@ -68,7 +74,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             '------'------'------'            '------'------'------'
      */
 
-    [SYM] = LAYOUT(KC_TILD, KC_PPLS, KC_AMPR, KC_ASTR, KC_LPRN, KC_LCBR, KC_RCBR, KC_RPRN, KC_QUOT, KC_COLN, KC_SCLN, _______, KC_GRV, KC_PEQL, KC_DLR, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, MRSFT, MRCTL, MRALT, MRGUI, KC_PIPE, KC_UNDS, KC_MINS, KC_EXLM, KC_AT, KC_HASH, KC_LT, KC_GT, KC_DQT, _______, _______, _______, KC_BSLS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+     [SYM] = LAYOUT(
+        KC_TILD, KC_PPLS, KC_AMPR, KC_ASTR, KC_LPRN, KC_LCBR,                     KC_RCBR, KC_RPRN, KC_QUOT, KC_COLN, KC_SCLN, _______,
+         KC_GRV, KC_PEQL,  KC_DLR, KC_PERC, KC_CIRC, KC_LBRC,                     KC_RBRC,   MRSFT,   MRCTL,   MRALT,   MRGUI, KC_PIPE,
+        KC_UNDS, KC_MINS, KC_EXLM,   KC_AT, KC_HASH,   KC_LT,                       KC_GT,  KC_DQT, _______, _______, _______, KC_BSLS,
+                          _______, _______,                                                         _______, _______,
+                                            _______, _______, _______,   _______, _______, _______
+    ),
 
     /* Nav Layer
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
@@ -84,7 +96,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             '------'------'------'            '------'------'------'
      */
 
-    [NAV] = LAYOUT(KC_ESC, KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_NO, _______, A(KC_TAB), MLGUI, MLALT, MLCTL, MLSFT, KC_VOLD, C(KC_LEFT), KC_LEFT, KC_DOWN, KC_RGHT, C(KC_RGHT), KC_DEL, KC_PSCR, KC_NO, KC_NO, KC_NO, KC_MSTP, KC_MUTE, KC_PGDN, S(KC_TAB), KC_NO, KC_TAB, KC_NO, KC_INS, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+    [NAV] = LAYOUT(
+         KC_ESC,   KC_NO, KC_MPRV, KC_MPLY, KC_MNXT, KC_VOLU,                     KC_PGUP,   KC_HOME,   KC_UP,  KC_END,      KC_NO, _______,
+      A(KC_TAB),   MLGUI,   MLALT,   MLCTL,   MLSFT, KC_VOLD,                  C(KC_LEFT),   KC_LEFT, KC_DOWN, KC_RGHT, C(KC_RGHT),  KC_DEL,
+        KC_PSCR,   KC_NO,   KC_NO,   KC_NO, KC_MSTP, KC_MUTE,                     KC_PGDN, S(KC_TAB),   KC_NO,  KC_TAB,      KC_NO,  KC_INS,
+                          _______, _______,                                                         _______, _______,
+                                            _______, _______, _______,   _______, _______, _______
+    ),
 
     /* Num Layer
      * ,------,------,------,------,------,------,                          ,------,------,------,------,------,------,
@@ -100,7 +118,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      *                             '------'------'------'            '------'------'------'
      */
 
-    [NUM] = LAYOUT(KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F10, KC_PPLS, KC_P7, KC_P8, KC_P9, KC_PAST, _______, KC_NUM, KC_NO, KC_F4, KC_F5, KC_F6, KC_F11, KC_PEQL, KC_P4, KC_P5, KC_P6, KC_COMM, KC_DEL, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, KC_PMNS, KC_P1, KC_P2, KC_P3, KC_PSLS, KC_PENT, _______, _______, KC_P0, KC_PDOT, _______, _______, _______, _______, _______, _______)};
+    [NUM] = LAYOUT(
+          KC_NO,   KC_NO,   KC_F7,   KC_F8,   KC_F9,  KC_F10,                     KC_PPLS,   KC_P7,   KC_P8,   KC_P9, KC_PAST, _______,
+         KC_NUM,   KC_NO,   KC_F4,   KC_F5,   KC_F6,  KC_F11,                     KC_PEQL,   KC_P4,   KC_P5,   KC_P6, KC_COMM,  KC_DEL,
+          KC_NO,   KC_NO,   KC_F1,   KC_F2,   KC_F3,  KC_F12,                     KC_PMNS,   KC_P1,   KC_P2,   KC_P3, KC_PSLS, KC_PENT,
+                          _______, _______,                                                           KC_P0, KC_PDOT,
+                                            _______, _______, _______,   _______, _______, _______
+    )
+}
 
 // Achordion
 bool process_record_user(uint16_t keycode, keyrecord_t* record) {
